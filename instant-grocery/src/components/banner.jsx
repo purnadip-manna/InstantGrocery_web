716fca4218f59c36bce1f6492e472d793ea87f27
Keder <kedernath.mallick.tint022@gmail.com>
2021-09-07 19:50:05 +0530

    'time_fixed'

--- instant-grocery/src/components/banner.jsx
+++ instant-grocery/src/components/banner.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 class Banner extends Component {
+    constructor(props){
+        super(props);
+        this.state={time:''};
+        this.getTimeNow=this.getTimeNow.bind(this);
+    }
     render() { 
         return (
             <React.Fragment>
-                <div className="row" id="banner">
+                <div className="row" id="banner" onLoad={this.getTimeNow}>
                     <div className="col-sm-2 order-sm-1">
-                        <img src="https://i.pinimg.com/originals/5d/26/a1/5d26a173f443cbd190e34481438d474b.png" id="shopLogo"></img>
+                        <img src="https://i.pinimg.com/originals/5d/26/a1/5d26a173f443cbd190e34481438d474b.png" id="shopLogo" alt="Logo"></img>
                     </div>
                     <div className="col-sm-8 order-sm-2">
                         <div className="jumbotron jumbotron-fluid">
@@ -16,21 +21,23 @@ class Banner extends Component {
                             </div>
                         </div>
                     </div>
-                    <div className="col-sm-2 order-sm-3">
-                        <p id="timeDiv" onLoad={this.getTimeNow()}>00</p>
+                    <div className="col-sm-2 order-sm-3" >
+                        <p id="timeDiv">{this.state.time}</p>
                     </div>
                 </div>
             </React.Fragment>
         );
+        
     }
-    getTimeNow = () =>{
+    
+    getTimeNow(e){
         var date = new Date();
         var h = date.getHours(); // 0 - 23
         var m = date.getMinutes(); // 0 - 59
         var s = date.getSeconds(); // 0 - 59
         var session = "AM";
         
-        if(h == 0){
+        if(h === 0){
             h = 12;
         }
         
@@ -43,11 +50,12 @@ class Banner extends Component {
         m = (m < 10) ? "0" + m : m;
         s = (s < 10) ? "0" + s : s;
         
-        var time = h + ":" + m + ":" + s + " " + session;
-        document.getElementById("timeDiv").innerText = time;
-        document.getElementById("timeDiv").textContent = time;
+        var times = h + ":" + m + ":" + s + " " + session;
         
+        
+        this.setState({time:times});
         setTimeout(this.getTimeNow, 1000);
+        
     }
 }
 

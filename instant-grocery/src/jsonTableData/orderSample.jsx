df56f533918db9124d6ebdf4932ecaed030c7332
Purnadip Manna <purnadipmanna2702@gmail.com>
2021-09-10 21:39:25 +0530

    major_upd_order_details

---
+++ instant-grocery/src/jsonTableData/orderSample.jsx
@@ -0,0 +1,63 @@
+const OrderSampleData=[{
+    "item_id": 1,
+    "item_name": "Coffee - Decafenated",
+    "quantity": 67,
+    "unit": "lorem ipsum",
+    "price": "$5.05"
+  }, {
+    "item_id": 2,
+    "item_name": "Lobster - Tail, 3 - 4 Oz",
+    "quantity": 36,
+    "unit": "adipiscing elit",
+    "price": "$9.94"
+  }, {
+    "item_id": 3,
+    "item_name": "Pork Loin Cutlets",
+    "quantity": 67,
+    "unit": "velit eu est",
+    "price": "$7.33"
+  }, {
+    "item_id": 4,
+    "item_name": "Beef - Tongue, Cooked",
+    "quantity": 17,
+    "unit": "nisi vulputate nonummy",
+    "price": "$9.74"
+  }, {
+    "item_id": 5,
+    "item_name": "Monkfish Fresh - Skin Off",
+    "quantity": 83,
+    "unit": "pretium",
+    "price": "$1.84"
+  }, {
+    "item_id": 6,
+    "item_name": "Beef - Inside Round",
+    "quantity": 88,
+    "unit": "lorem id",
+    "price": "$1.27"
+  }, {
+    "item_id": 7,
+    "item_name": "Greens Mustard",
+    "quantity": 16,
+    "unit": "felis",
+    "price": "$8.61"
+  }, {
+    "item_id": 8,
+    "item_name": "Lotus Rootlets - Canned",
+    "quantity": 46,
+    "unit": "sed magna at",
+    "price": "$5.56"
+  }, {
+    "item_id": 9,
+    "item_name": "Kellogs Raisan Bran Bars",
+    "quantity": 29,
+    "unit": "sit",
+    "price": "$7.37"
+  }, {
+    "item_id": 10,
+    "item_name": "Sultanas",
+    "quantity": 77,
+    "unit": "nunc donec quis",
+    "price": "$8.64"
+  }];
+
+export default OrderSampleData;

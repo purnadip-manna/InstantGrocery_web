06210bc8746bb5a9c792ad5694bee6ae156fdf3e
Purnadip Manna <purnadipmanna2702@gmail.com>
2021-10-26 16:35:02 +0530

    Sidebar_updated

--- src/components/mainArea.jsx
+++ src/components/mainArea.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import Banner from "./banner";
 import AddOrders from './addOrder';
-import PantryDetails from './pantryDetails'
+import PantryDetails from './pantryDetails';
 import ShowOrders from './showOrders';
+import Categories from './categories';
+
 import {
     BrowserRouter as Router,
     Switch,
@@ -29,6 +31,11 @@ const routes = [
     main: () => <PantryDetails/>
 },
 {
+    path: "/dashboard/categories",
+    sidebar: () => <div>shoelaces!</div>,
+    main: () => <Categories/>
+},
+{
     path:"/dashboard/refill",
     sidebar:()=><div></div>,
     main : ()=><RefillItems/>
@@ -50,7 +57,7 @@ class MainArea extends Component {
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bag-check mb-1 mr-2" viewBox="0 0 16 16">
                                     <path fillRule="evenodd" d="M10.854 8.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
                                     <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                                </svg> Order Details<span className="badge badge-success ml-2">69</span>
+                                </svg> Order Details<span className="badge badge-success ml-2">9</span>
                             </span>
                         </Link>
 
@@ -71,6 +78,15 @@ class MainArea extends Component {
                                 </svg> Pantry Details
                             </span>
                         </Link>
+
+                        <Link to="/dashboard/categories">
+                            <span className="list-group-item list-group-item-action list-group-item-light">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-back mb-1 mr-2" viewBox="0 0 16 16">
+                            <path d="M0 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v2h2a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-2H2a2 2 0 0 1-2-2V2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H2z"/>
+                            </svg> Categories
+                            </span>
+                        </Link>
+
                         <Link to="/dashboard/refill">
                             <span className="list-group-item list-group-item-action list-group-item-light">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bag-plus mb-1 mr-2" viewBox="0 0 16 16">
